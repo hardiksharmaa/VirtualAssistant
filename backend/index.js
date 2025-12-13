a396855f92a8110ce4fe8f1b7d95a7a050f75f82
Hardik <hs489819@gmail.com>
2025-12-14 00:51:00 +0530

    askdn

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://jarvis-qlvo.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
